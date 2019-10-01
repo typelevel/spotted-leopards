46a538df1c59e513718d57a5874656cbc3e394c4
mikolaj <mikolaj.szabo@tundra.com>
2019-10-01 16:42:26 +0200

    Upgraded to Dotty 0.19.0-RC1

--- src/main/scala/leopards/instances/list.scala
+++ src/main/scala/leopards/instances/list.scala
@@ -1,11 +1,11 @@
 package leopards
 
-delegate for Monad[List], Traverse[List] {
+given Monad[List], Traverse[List] {
   def (a: A) pure[A]: List[A] = List(a)
   def (fa: List[A]) flatMap[A, B](f: A => List[B]): List[B] =
     fa.flatMap(f)
   def (fa: List[A]) foldLeft[A, B](b: B)(f: (B, A) => B): B =
     fa.foldLeft(b)(f)
-  def (fa: List[A]) traverse[G[_], A, B](f: A => G[B]) given (G: Applicative[G]): G[List[B]] =
+  def (fa: List[A]) traverse[G[_], A, B](f: A => G[B]) (given G: Applicative[G]): G[List[B]] =
     fa.foldRight(G.pure(List.empty[B]))((a, acc) => f(a).map2(acc)(_ :: _))
 }
