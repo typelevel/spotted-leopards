a60d0c3d7bcae639ac3333ac1abd911ae5e4c394
Ben Plommer <ben.plommer@itv.com>
2021-05-06 17:58:36 +0100

    Add FunctionK

--- src/test/scala/leopards/OptionTTest.scala
+++ src/test/scala/leopards/OptionTTest.scala
@@ -8,6 +8,29 @@ class OptionTTest extends FunSuite:
     assertEquals(x.value, List(Some(42)))
   }
 
+  test("mapK") {
+    val fk: List ~> Option = [X] => (_: List[X]).headOption
+
+    val x: OptionT[List, Int] = OptionT(List(Some(3), Some(4)))
+    assertEquals(OptionT.mapK[List, Int](x)(fk).value, Some(Some(3)))
+
+    // x.mapK(fk)
+    //
+    // Fails to compile with:
+    // [error] -- [E007] Type Mismatch Error: spotted-leopards/src/test/scala/leopards/OptionTTest.scala:71:11 
+    // [error] 71 |    x.mapK(fk)
+    // [error]    |           ^^
+    // [error]    |Found:    (fk : leopards.FunctionK[List, Option])
+    // [error]    |Required: leopards.FunctionK[F, G]
+    // [error]    |
+    // [error]    |where:    F is a type variable with constraint >: List and <: [_$6] =>> Any
+    // [error]    |          G is a type variable with constraint <: [_$7] =>> Any
+  }
+
+  test("liftK") {
+    assertEquals(OptionT.liftK[List](List(42)), OptionT.liftF(List(42)))
+  }
+
   test("map") {
     val x = OptionT.fromOption[List, Int](Some(42))
 // https://github.com/typelevel/spotted-leopards/issues/1
